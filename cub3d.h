219abea7dec6ddba0d0f00e00b405feb3fccebed
cardano04class <raja_amir@outlook.fr>
2025-03-21 04:37:27 +0000

    window cration & data init

--- cub3d.h
+++ cub3d.h
@@ -1,7 +1,65 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# define SCREEN_WIDTH 1900
+# define SCREEN_HEIGHT 1000
+# define FOV 60
+# define TILE_SIZE 60
+# define ROTATE_SPEED 0.045
+# define MOVE_SPEED 4
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <limits.h>
 # include <mlx.h>
+
+typedef struct s_player
+{
+    int player_x;
+    int player_y;
+    double player_angle;
+    float fov_rd;
+    int rot;
+    int l_r;
+    int u_d;
+} t_player;
+
+typedef struct s_ray
+{
+    double ray_angle;
+    double distance;
+    int flag;
+} t_ray;
+
+typedef struct s_mlx
+{
+    void *mlx;
+    void *win;
+    void *img;
+} t_mlx;
+typedef struct s_map
+{
+    char **map;
+
+    int fd;
+    int line_count;
+    char *path;
+    char **file;
+    int index;
+    int width;
+    int height;
+} t_map;
+typedef struct s_data
+{
+    t_map *map;
+    int win_width;
+    int win_height;
+    t_mlx *mlx;
+    t_player *player;
+    t_ray *ray;
+} t_data;
 
 #endif
--- main.c
+++ main.c
@@ -1,33 +1,76 @@
-#include <mlx.h>
+#include "cub3d.h"
+
+
+void init_data(t_data *data)
+{
+    data->player = (t_player *)malloc(sizeof(t_player));
+    data->ray = (t_ray *)malloc(sizeof(t_ray));
+    data->mlx = (t_mlx *)malloc(sizeof(t_mlx));
+    data->map = (t_map *)malloc(sizeof(t_map));
+
+    data->mlx->mlx = NULL;
+    data->mlx->win = NULL;
+    data->mlx->img = NULL;
+    data->win_height = SCREEN_HEIGHT;
+    data->win_width =  SCREEN_WIDTH;
+}
+
+void init_mlx(t_data *data)
+{
+    data->mlx->mlx = mlx_init();
+    if (!data->mlx->mlx)
+    {
+        printf("Error\nmlx_init failed\n");
+        exit(1);
+    }
+    data->mlx->win = mlx_new_window(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
+    if (!data->mlx->win)
+    {
+        printf("Error\nmlx_new_window failed\n");
+        exit(1);
+    }
+    data->mlx->img = mlx_new_image(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+    if (!data->mlx->img)
+    {
+        printf("Error\nmlx_new_image failed\n");
+        exit(1);
+    }
+    mlx_put_image_to_window(data->mlx->mlx, data->mlx->win, data->mlx->img, 0, 0);
+    return;
+}
+
+int close_window(t_data *data)
+{
+    mlx_destroy_window(data->mlx->mlx, data->mlx->win);
+    printf("Exited Game\n");
+    exit(0);
+    return (0);
+}
+
+int key_press(int keycode, t_data *data)
+{
+    printf("Key pressed: %d\n", keycode);
+    if (keycode == 65307)
+    {
+        printf("Exited Game!\n");
+        close_window(data);
+    }
+    return (0);
+}
+
+void start_game(t_data *data)
+{
+    init_mlx(data);
+    mlx_hook(data->mlx->win, 2, 1L<<0, key_press, data); // Listen for key press events
+    mlx_hook(data->mlx->win, 17, 0, close_window, data); // Listen for window close events
+    mlx_loop(data->mlx->mlx);
+    return;
+}
 
 int main()
 {
-    void    *mlx;
-    void    *mlx_win;
-    int     x, y;
-    int     border_size = 10; // Size of the border
-    int     square_size = 100; // Size of the square
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Cub3D");
-
-    // Draw the square with a border
-    for (x = 100; x < 100 + square_size; x++) 
-	{
-        for (y = 100; y < 100 + square_size; y++) 
-		{
-            // Check if the pixel is within the border
-            if (x < 100 + border_size || x >= 100 + square_size - border_size || 
-                y < 100 + border_size || y >= 100 + square_size - border_size) 
-			{
-                mlx_pixel_put(mlx, mlx_win, x, y, 0xFF0000); // Red border
-            }
-			else 
-			{
-                mlx_pixel_put(mlx, mlx_win, x, y, 0x00FF00); // Green fill
-            }
-        }
-    }
-    mlx_loop(mlx);
-    return 0;
+    t_data data;
+    init_data(&data);
+    start_game(&data);
+    return (0);
 }
