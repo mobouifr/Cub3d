bcc007ce366957ab6a9ab1175f0f5a7fe32b9bca
Mamir <raja_amir@outlook.fr>
2025-03-22 02:44:38 +0000

    Revert "new stuff"
    This reverts commit 885fcf38bd4a038117741bd977816466c8e24fda.

--- cub3d.h
+++ cub3d.h
@@ -1,10 +1,13 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define SCREEN_WIDTH 640
-# define SCREEN_HEIGHT 480
+# define SCREEN_WIDTH 600
+# define SCREEN_HEIGHT 600
+# define FOV 60
+# define TILE_SIZE 60
+# define ROTATE_SPEED 0.045
+# define MOVE_SPEED 4
 # include <stdio.h>
-# include <stdbool.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
@@ -13,23 +16,23 @@
 # include <limits.h>
 # include <mlx.h>
 # include "get_next_line/get_next_line.h"
-
 typedef struct s_player
 {
-    double player_x;
-    double player_y;
-    double dir_x;
-    double dir_y;
-    double plane_x;
-    double plane_y;
+    int player_x;
+    int player_y;
+    double player_angle;
+    float fov_rd;
+    int rot;
+    int l_r;
+    int u_d;
 } t_player;
 
-typedef struct s_map
+typedef struct s_ray
 {
-    int width;
-    int height;
-    char **map;
-} t_map;
+    double ray_angle;
+    double distance;
+    int flag;
+} t_ray;
 
 typedef struct s_mlx
 {
@@ -37,19 +40,26 @@ typedef struct s_mlx
     void *win;
     void *img;
 } t_mlx;
+typedef struct s_map
+{
+    char **map;
 
+    int fd;
+    int line_count;
+    char *path;
+    char **file;
+    int index;
+    int width;
+    int height;
+} t_map;
 typedef struct s_data
 {
-    t_player *player;
     t_map *map;
+    int win_width;
+    int win_height;
     t_mlx *mlx;
-    bool keys[256];
+    t_player *player;
+    t_ray *ray;
 } t_data;
-
-void init_data(t_data *data, const char *filename);
-void start_game(t_data *data);
-bool can_move_to(t_map *map, int x, int y);
-double get_time();
-void main_loop(t_data *data);
 
 #endif
