885fcf38bd4a038117741bd977816466c8e24fda
Mo.Amir <macbook@Macbook-air-Mamir.local>
2025-03-22 01:17:13 +0000

    new stuff

--- cub3d.h
+++ cub3d.h
@@ -1,13 +1,10 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define SCREEN_WIDTH 600
-# define SCREEN_HEIGHT 600
-# define FOV 60
-# define TILE_SIZE 60
-# define ROTATE_SPEED 0.045
-# define MOVE_SPEED 4
+# define SCREEN_WIDTH 640
+# define SCREEN_HEIGHT 480
 # include <stdio.h>
+# include <stdbool.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
@@ -16,23 +13,23 @@
 # include <limits.h>
 # include <mlx.h>
 # include "get_next_line/get_next_line.h"
+
 typedef struct s_player
 {
-    int player_x;
-    int player_y;
-    double player_angle;
-    float fov_rd;
-    int rot;
-    int l_r;
-    int u_d;
+    double player_x;
+    double player_y;
+    double dir_x;
+    double dir_y;
+    double plane_x;
+    double plane_y;
 } t_player;
 
-typedef struct s_ray
+typedef struct s_map
 {
-    double ray_angle;
-    double distance;
-    int flag;
-} t_ray;
+    int width;
+    int height;
+    char **map;
+} t_map;
 
 typedef struct s_mlx
 {
@@ -40,26 +37,19 @@ typedef struct s_mlx
     void *win;
     void *img;
 } t_mlx;
-typedef struct s_map
-{
-    char **map;
 
-    int fd;
-    int line_count;
-    char *path;
-    char **file;
-    int index;
-    int width;
-    int height;
-} t_map;
 typedef struct s_data
 {
-    t_map *map;
-    int win_width;
-    int win_height;
-    t_mlx *mlx;
     t_player *player;
-    t_ray *ray;
+    t_map *map;
+    t_mlx *mlx;
+    bool keys[256];
 } t_data;
+
+void init_data(t_data *data, const char *filename);
+void start_game(t_data *data);
+bool can_move_to(t_map *map, int x, int y);
+double get_time();
+void main_loop(t_data *data);
 
 #endif
--- main.c
+++ main.c
@@ -1,183 +1,95 @@
 #include "cub3d.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <mlx.h>
+#include <math.h>
+#include <sys/time.h>
 
-int read_lines(int fd)
+void init_mlx(void **mlx, void **mlx_win)
 {
-    int line_count;
-    char *line;
-    line_count = 0;
-    while (get_next_line(fd) > 0)
-    {
-        line_count++;
-        free(line);
-    }
-    return (line_count);
+    *mlx = mlx_init();
+    *mlx_win = mlx_new_window(*mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "raycaster!");
+    mlx_loop(*mlx);
 }
 
-int get_map_width(char *line)
+double get_time()
 {
-    int i;
-    int width;
-    i = 0;
-    width = 0;
-    while (line[i])
-    {
-        if (line[i] == '1')
-            width++;
-        i++;
-    }
-    return (width);
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    return (time.tv_sec + (time.tv_usec / 1000000.0));
 }
 
-int get_map_height(char **file, int line_count)
+void main_loop(t_data *data)
 {
-    int i;
-    int height;
-    i = 0;
-    height = 0;
-    while (i < line_count)
+    static double oldTime = 0;
+    double time = get_time();
+    double frameTime = time - oldTime;
+    oldTime = time;
+
+    // Speed modifiers
+    double moveSpeed = frameTime * 5.0; // The constant value is in squares/second
+    double rotSpeed = frameTime * 3.0; // The constant value is in radians/second
+
+    // Event handling
+    if (data->keys['w']) // Move forward
     {
-        if (file[i][0] == '1')
-            height++;
-        i++;
+        if (can_move_to(data->map, (int)(data->player->player_x + data->player->dir_x * moveSpeed), (int)(data->player->player_y)))
+            data->player->player_x += data->player->dir_x * moveSpeed;
+        if (can_move_to(data->map, (int)(data->player->player_x), (int)(data->player->player_y + data->player->dir_y * moveSpeed)))
+            data->player->player_y += data->player->dir_y * moveSpeed;
     }
-    return (height);
-}
-
-void fill_map(t_data *data)
-{
-    int i;
-    int j;
-    int k;
-    i = 0;
-    j = 0;
-    k = 0;
-    data->map->map = (char **)malloc(sizeof(char *) * data->map->height);
-    while (i < data->map->height)
+    if (data->keys['s']) // Move backward
     {
-        data->map->map[i] = (char *)malloc(sizeof(char) * data->map->width);
-        while (j < data->map->width)
-        {
-            data->map->map[i][j] = data->map->file[k][j];
-            j++;
-        }
-        j = 0;
-        k++;
-        i++;
+        if (can_move_to(data->map, (int)(data->player->player_x - data->player->dir_x * moveSpeed), (int)(data->player->player_y)))
+            data->player->player_x -= data->player->dir_x * moveSpeed;
+        if (can_move_to(data->map, (int)(data->player->player_x), (int)(data->player->player_y - data->player->dir_y * moveSpeed)))
+            data->player->player_y -= data->player->dir_y * moveSpeed;
     }
-}
-
-void read_map_file(t_data *data, const char *filename)
-{
-    int i = 0;
-    char *line;
-    char buffer[1024];
-    int bytes_read;
-    int line_length;
-    int buffer_index;
-    int fd;
-
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
+    if (data->keys['a']) // Rotate left
     {
-        perror("Error\nopen failed");
-        exit(1);
+        double oldDirX = data->player->dir_x;
+        data->player->dir_x = data->player->dir_x * cos(rotSpeed) - data->player->dir_y * sin(rotSpeed);
+        data->player->dir_y = oldDirX * sin(rotSpeed) + data->player->dir_y * cos(rotSpeed);
+        double oldPlaneX = data->player->plane_x;
+        data->player->plane_x = data->player->plane_x * cos(rotSpeed) - data->player->plane_y * sin(rotSpeed);
+        data->player->plane_y = oldPlaneX * sin(rotSpeed) + data->player->plane_y * cos(rotSpeed);
+    }
+    if (data->keys['d']) // Rotate right
+    {
+        double oldDirX = data->player->dir_x;
+        data->player->dir_x = data->player->dir_x * cos(-rotSpeed) - data->player->dir_y * sin(-rotSpeed);
+        data->player->dir_y = oldDirX * sin(-rotSpeed) + data->player->dir_y * cos(-rotSpeed);
+        double oldPlaneX = data->player->plane_x;
+        data->player->plane_x = data->player->plane_x * cos(-rotSpeed) - data->player->plane_y * sin(-rotSpeed);
+        data->player->plane_y = oldPlaneX * sin(-rotSpeed) + data->player->plane_y * cos(-rotSpeed);
     }
 
-    data->map->file = (char **)malloc(sizeof(char *) * data->map->line_count);
+    // Raycasting and rendering
+    // Implement raycasting and rendering logic here
 
-    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0)
-    {
-        buffer[bytes_read] = '\0';
-        buffer_index = 0;
-        while (buffer[buffer_index])
-        {
-            line_length = 0;
-            while (buffer[buffer_index + line_length] && buffer[buffer_index + line_length] != '\n')
-                line_length++;
-            line = (char *)malloc(sizeof(char) * (line_length + 1));
-            for (int j = 0; j < line_length; j++)
-                line[j] = buffer[buffer_index + j];
-            line[line_length] = '\0';
-            data->map->file[i] = line;
-            i++;
-            buffer_index += line_length + 1;
-        }
-    }
-    data->map->file[i] = NULL;
-    close(fd);
-}
-
-void init_data(t_data *data, const char *filename)
-{
-    data->map = (t_map *)malloc(sizeof(t_map));
-    data->map->fd = open(filename, O_RDONLY);
-    if (data->map->fd < 0)
-    {
-        perror("Error\nopen failed");
-        exit(1);
-    }
-    data->map->line_count = read_lines(data->map->fd);
-    close(data->map->fd);
-    read_map_file(data, filename);
-    data->map->width = get_map_width(data->map->file[0]);
-    data->map->height = get_map_height(data->map->file, data->map->line_count);
-    fill_map(data);
-
-    data->player = (t_player *)malloc(sizeof(t_player));
-    data->ray = (t_ray *)malloc(sizeof(t_ray));
-    data->mlx = (t_mlx *)malloc(sizeof(t_mlx));
-
-    data->player->player_x = 5;
-    data->player->player_y = 4;
-    data->mlx->mlx = NULL;
-    data->mlx->win = NULL;
-    data->mlx->img = NULL;
-    data->win_height = SCREEN_HEIGHT;
-    data->win_width = SCREEN_WIDTH;
-}
-
-void init_mlx(t_data *data)
-{
-    data->mlx->mlx = mlx_init();
-    if (!data->mlx->mlx)
-    {
-        printf("Error\nmlx_init failed\n");
-        exit(1);
-    }
-    data->mlx->win = mlx_new_window(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
-    if (!data->mlx->win)
-    {
-        printf("Error\nmlx_new_window failed\n");
-        exit(1);
-    }
-    data->mlx->img = mlx_new_image(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if (!data->mlx->img)
-    {
-        printf("Error\nmlx_new_image failed\n");
-        exit(1);
-    }
+    // Update the window
+    mlx_clear_window(data->mlx->mlx, data->mlx->win);
     mlx_put_image_to_window(data->mlx->mlx, data->mlx->win, data->mlx->img, 0, 0);
-    return;
+    mlx_do_sync(data->mlx->mlx);
 }
-bool can_move_to(t_map *map, int x, int y) {
-    if (x < 0 || x >= map->width || y < 0 || y >= map->height) {
+
+bool can_move_to(t_map *map, int x, int y)
+{
+    if (x < 0 || x >= map->width || y < 0 || y >= map->height)
         return false;
-    }
     return map->map[y][x] != '1';
 }
 
-void move_player(t_player *player, t_map *map, double dx, double dy) {
-    double new_x = player->player_x + dx;
-    double new_y = player->player_y + dy;
-    int map_x = (int)new_x;
-    int map_y = (int)new_y;
-    if (can_move_to(map, map_x, map_y)) {
-        player->player_x = new_x;
-        player->player_y = new_y;
-    }
+int key_press(int keycode, t_data *data)
+{
+    if (keycode < 256)
+        data->keys[keycode] = true;
+    return 0;
+}
+
+int key_release(int keycode, t_data *data)
+{
+    if (keycode < 256)
+        data->keys[keycode] = false;
+    return 0;
 }
 
 int close_window(t_data *data)
@@ -185,120 +97,62 @@ int close_window(t_data *data)
     mlx_destroy_window(data->mlx->mlx, data->mlx->win);
     printf("Exited Game\n");
     exit(0);
-    return (0);
+    return 0;
 }
 
-void draw_square(t_data *data, int x, int y, int color, int square_size)
+void init_data(t_data *data, const char *filename)
 {
-    int i, j;
-    char *pixel;
-    int bpp, size_line, endian;
+    data->map = (t_map *)malloc(sizeof(t_map));
+    data->player = (t_player *)malloc(sizeof(t_player));
+    data->mlx = (t_mlx *)malloc(sizeof(t_mlx));
 
-    pixel = mlx_get_data_addr(data->mlx->img, &bpp, &size_line, &endian);
-    for (i = 0; i < square_size; i++)
+    // Initialize player position and direction
+    data->player->player_x = 22;
+    data->player->player_y = 12;
+    data->player->dir_x = -1;
+    data->player->dir_y = 0;
+    data->player->plane_x = 0;
+    data->player->plane_y = 0.66;
+
+    // Initialize map
+    data->map->width = 24;
+    data->map->height = 24;
+    data->map->map = (char **)malloc(sizeof(char *) * data->map->height);
+    for (int i = 0; i < data->map->height; i++)
     {
-        for (j = 0; j < square_size; j++)
+        data->map->map[i] = (char *)malloc(sizeof(char) * data->map->width);
+        for (int j = 0; j < data->map->width; j++)
         {
-            int pixel_index = ((y + i) * size_line) + ((x + j) * (bpp / 8));
-            if (i == 0 || j == 0 || i == square_size - 1 || j == square_size - 1)
-                *(int *)(pixel + pixel_index) = 0x000000; // Draw border
+            if (i == 0 || i == data->map->height - 1 || j == 0 || j == data->map->width - 1)
+                data->map->map[i][j] = '1';
             else
-                *(int *)(pixel + pixel_index) = color;
+                data->map->map[i][j] = '0';
         }
     }
-}
 
-void draw_map(t_data *data)
-{
-    int i, j;
-    int x, y;
-    int square_width = SCREEN_WIDTH / data->map->width;
-    int square_height = SCREEN_HEIGHT / data->map->height;
-    int square_size = (square_width < square_height) ? square_width : square_height;
+    // Initialize keys
+    for (int i = 0; i < 256; i++)
+        data->keys[i] = false;
 
-    // Clear the image before drawing
-    int bpp, size_line, endian;
-    char *pixel = mlx_get_data_addr(data->mlx->img, &bpp, &size_line, &endian);
-    memset(pixel, 0, SCREEN_WIDTH * SCREEN_HEIGHT * (bpp / 8));
-
-    for (i = 0; i < data->map->height; i++)
-    {
-        for (j = 0; j < data->map->width; j++)
-        {
-            x = j * square_size;
-            y = i * square_size;
-            if (data->map->map[i][j] == '1')
-                draw_square(data, x, y, 0x00FF00, square_size); // Green for walls
-            else if (data->map->map[i][j] == '0')
-                draw_square(data, x, y, 0xFFFFFF, square_size); // White for empty space
-        }
-    }
-    draw_square(data, (int)(data->player->player_x * square_size), (int)(data->player->player_y * square_size), 0xFF0000, square_size / 2);
-    mlx_put_image_to_window(data->mlx->mlx, data->mlx->win, data->mlx->img, 0, 0);
-}
-
-int key_press(int keycode, t_data *data)
-{
-    double move_speed = 0.1; // Adjust the speed for smoother movement
-    if (keycode == 65307) // Escape key
-    {
-        printf("Exited Game!\n");
-        close_window(data);
-    }
-    else if (keycode == 'w') // Move up
-    {
-        move_player(data->player, data->map, 0, -move_speed);
-    }
-    else if (keycode == 'a') // Move left
-    {
-        move_player(data->player, data->map, -move_speed, 0);
-    }
-    else if (keycode == 's') // Move down
-    {
-        move_player(data->player, data->map, 0, move_speed);
-    }
-    else if (keycode == 'd') // Move right
-    {
-        move_player(data->player, data->map, move_speed, 0);
-    }
-    draw_map(data); // Redraw the map with the new player position
-    return (0);
-}
-
-int key_release(int keycode, t_data *data)
-{
-    // Handle key release events if needed
-    return (0);
-}
-
-int update(t_data *data)
-{
-    draw_map(data); // Continuously redraw the map
-    return (0);
+    // Initialize mlx
+    data->mlx->mlx = mlx_init();
+    data->mlx->win = mlx_new_window(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
+    data->mlx->img = mlx_new_image(data->mlx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 void start_game(t_data *data)
 {
-    init_mlx(data);
-    draw_map(data); // Draw the map when the game starts
+    mlx_loop_hook(data->mlx->mlx, (int (*)())main_loop, data);
     mlx_hook(data->mlx->win, 2, 1L<<0, key_press, data); // Listen for key press events
     mlx_hook(data->mlx->win, 3, 1L<<1, key_release, data); // Listen for key release events
-    mlx_loop_hook(data->mlx->mlx, update, data); // Continuously update the game
     mlx_hook(data->mlx->win, 17, 0, close_window, data); // Listen for window close events
     mlx_loop(data->mlx->mlx);
-    return;
 }
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    if (ac != 2)
-    {
-        printf("Usage: %s <map_file>\n", av[0]);
-        return (1);
-    }
-
     t_data data;
-    init_data(&data, av[1]);
+    init_data(&data, argv[1]);
     start_game(&data);
     return (0);
 }
