7588a34381fcc3443278e2781b9190b7755f34f1
mobouifr <125811900+mobouifr@users.noreply.github.com>
2025-04-27 14:49:47 +0000

    added t_game struct o the header file, and initialised it

--- parser/parse.c
+++ parser/parse.c
@@ -11,9 +11,38 @@ void    skip_empty_line(int fd)
     }
 }
 
+void    var_init(t_game* gamevar)
+{
+    int i = 0;
+    
+    gamevar->no_path = NULL;
+    gamevar->so_path = NULL;
+    gamevar->we_path = NULL;
+    gamevar->ea_path = NULL;
+    while(i < 3)
+    {
+        gamevar->floor_color[i] = -1;
+        gamevar->ceiling_color[i] = -1;
+        i++;
+    }
+    gamevar->map = NULL
+    gamevar->map_width = 0;
+    gamevar->map_heigth = 0;
+    gamevar->player_x = -1;
+    gamevar->player_y = -1;
+    gamevar->player_dir = '\0';
+    game->has_no = 0;
+    game->has_so = 0;
+    game->has_we = 0;
+    game->has_ea = 0;
+    game->has_floor = 0;
+    game->has_ceiling = 0;
+}
+
 int parser(int argc, char **argv)
 {
     int fd;
+    t_game* gamevar;
 
     if (argc != 2)
     {
@@ -25,7 +54,7 @@ int parser(int argc, char **argv)
         texture_files_check()
         {}
     */
-
+    var_init(&gamevar);
     fd = open("map.cub", O_RDONLY);
     if (fd == -1 /* || !valid_extention_check()*/)
     {
--- parser/parser.h
+++ parser/parser.h
@@ -5,7 +5,24 @@
 
 typedef struct s_game
 {
-    char *mapfilename;
+    char *no_path;
+    char *so_path;
+    char *we_path;
+    char *ea_path;
+    int floor_color[3];
+    int ceiling_color[3];
+    char **map;
+    int map_width;
+    int map_height;
+    int player_x;
+    int player_y;
+    char player_dir;
+    int has_no;
+    int has_so;
+    int has_we;
+    int has_ea;
+    int has_floor;
+    int has_ceiling;
 } t_game;
 
 #endif
